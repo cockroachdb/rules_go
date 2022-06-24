5a4682cd1eda7e7308107f3ff4adb981a81a953c
Ricky Stewart <ricky@cockroachlabs.com>
2022-06-24 16:44:41 -0500

    record results of `unused` lint in `.x` archives next to `nogo` output
    We need a way to track which objects/types are used and unused as we
    compile stuff. The `.x` archives contain `nogo` information on a per-
    package basis; next to the `nogo.out` file in each archive we also add
    a `unused.out` file that contains a JSON description of the used and
    unused objects for that package.
    
    1. Update `go/tools/builders/nogo_main.go` with a new `-u` argument. If
       provided, `nogo` will check the results of the `unused` lint and dump
       a slightly munged version of the output to the file. We exclude
       from these files cgo-related stuff and anything that doesn't come
       from `cockroachdb/cockroch`.
    2. Update `compile.go` and `compilepkg.go` to pass `-u` and to include
       this in the `.x` archive.
    3. Update `archive.bzl` and `binary.bzl` to capture the location of
       re-compiled `internal` archives for tests in the `GoArchiveData`.
       We'll traverse this to enumerate all the `.x` archives for test
       targets back in `cockroach`.

--- go/tools/builders/compilepkg.go
+++ go/tools/builders/compilepkg.go
@@ -404,11 +404,12 @@ func compileArchive(
 	// Run nogo concurrently.
 	var nogoChan chan error
 	outFactsPath := filepath.Join(workDir, nogoFact)
+	outUnusedPath := filepath.Join(workDir, "unused.out")
 	if nogoPath != "" {
 		ctx, cancel := context.WithCancel(context.Background())
 		nogoChan = make(chan error)
 		go func() {
-			nogoChan <- runNogo(ctx, workDir, nogoPath, goSrcs, deps, packagePath, importcfgPath, outFactsPath)
+			nogoChan <- runNogo(ctx, workDir, nogoPath, goSrcs, deps, packagePath, importcfgPath, outFactsPath, outUnusedPath)
 		}()
 		defer func() {
 			if nogoChan != nil {
@@ -497,7 +498,7 @@ func compileArchive(
 	}
 	pkgDefPath := filepath.Join(workDir, pkgDef)
 	if nogoStatus == nogoSucceeded {
-		return appendFiles(goenv, outXPath, []string{pkgDefPath, outFactsPath})
+		return appendFiles(goenv, outXPath, []string{pkgDefPath, outFactsPath, outUnusedPath})
 	}
 	return appendFiles(goenv, outXPath, []string{pkgDefPath})
 }
@@ -522,7 +523,7 @@ func compileGo(goenv *env, srcs []string, packagePath, importcfgPath, embedcfgPa
 	return goenv.runCommandAndReplacePaths(args, paths)
 }
 
-func runNogo(ctx context.Context, workDir string, nogoPath string, srcs []string, deps []archive, packagePath, importcfgPath, outFactsPath string) error {
+func runNogo(ctx context.Context, workDir string, nogoPath string, srcs []string, deps []archive, packagePath, importcfgPath, outFactsPath, outUnusedPath string) error {
 	args := []string{nogoPath}
 	args = append(args, "-p", packagePath)
 	args = append(args, "-importcfg", importcfgPath)
@@ -530,6 +531,7 @@ func runNogo(ctx context.Context, workDir string, nogoPath string, srcs []string
 		args = append(args, "-fact", fmt.Sprintf("%s=%s", dep.importPath, dep.file))
 	}
 	args = append(args, "-x", outFactsPath)
+	args = append(args, "-u", outUnusedPath)
 	args = append(args, srcs...)
 
 	paramsFile := filepath.Join(workDir, "nogo.param")
